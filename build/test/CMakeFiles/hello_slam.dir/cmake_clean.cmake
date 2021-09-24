8738289114ef24fbe79bc34f9205cb2a9564135c
sans114 <22032009@zju.edu.cn>
2021-09-24 16:48:38 +0800

    add

---
+++ build/test/CMakeFiles/hello_slam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_slam.dir/hello_slam.cpp.o"
+  "../../bin/hello_slam.pdb"
+  "../../bin/hello_slam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_slam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
